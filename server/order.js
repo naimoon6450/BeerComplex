49cf4efd7169786e395431141642ad1f8fbca3e0
Drew <dr3mit@gmail.com>
2019-07-22 20:23:36 -0400

    test folder structure and order model

--- server/order.js
+++ server/order.js
@@ -1,6 +1,18 @@
 const Sequelize = require("sequelize");
 const db = require("../index");
 
-db.define("Order", () => {
-  return;
+db.define("Order", {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true
+  },
+  orderTotal: {
+    type: Sequelize.DECIMAL(10, 2),
+    defaultValue: 0.0,
+    validate: {
+      isDecimal: true,
+      min: 0
+    }
+  }
 });
