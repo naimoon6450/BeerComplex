2947bf245cfc244580cb54c0d8c92042ced5ef0d
Drew <dr3mit@gmail.com>
2019-07-23 01:21:12 -0400

    added id to user

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 const db = require('../connection');
 
 const User = db.define('user', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
   firstName: {
     type: Sequelize.STRING,
     allowNull: false,
