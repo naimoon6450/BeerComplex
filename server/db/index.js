fa9098fb020b8a99212b7f8f924954b7618dba75
Dylan Levene <levene.dylan@gmail.com>
2019-07-22 20:15:23 -0400

    Supplier and Category Models

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,13 @@
 const db = require('./connection');
 const Product = require('../db/models/Product');
+const Supplier = require('../db/models/Supplier');
+const Category = require('../db/models/Category');
 
 // Associations Here
+Product.belongsTo(Supplier);
+Product.belongsTo(Category);
+Supplier.hasMany(Product);
+Category.hasMany(Product);
 
 module.exports = {
     db,
