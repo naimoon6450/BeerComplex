ffde514e395a8393bddff86e185fb4d20444d07e
Dylan Levene <levene.dylan@gmail.com>
2019-07-29 22:02:49 -0400

    router problemz

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-router.use('/products', require('./routers/ProductRouter'));
-router.use('/orders', require('./routers/OrderRouter'));
-router.use('/users', require('./routers/UserRouter'));
+router.use('/products', require('./router/ProductRouter'));
+router.use('/orders', require('./router/OrderRouter'));
+router.use('/users', require('./router/UserRouter'));
 
 router.use((req, res, next) => {
   res.status(404).send('Not found');
