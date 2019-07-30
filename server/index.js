90f9c5585cc59d4365ae2f4e4fa1619b4b55de81
Dylan Levene <levene.dylan@gmail.com>
2019-07-29 22:07:49 -0400

    PORT fix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db/connection');
-const PORT = process.env.HOST_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const { seed } = require('../seed');
 const morgan = require('morgan');
