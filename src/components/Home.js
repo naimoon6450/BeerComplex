06a41b9d1172de9573d2bcf14998b5de9b0bd057
naimoon6450 <naimoon6450@gmail.com>
2019-07-31 15:29:27 -0400

    update to /home upon refresh

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Home = props => {
-  const { user } = props.history.location.state;
-  return user.length ? <div>{`GREETINGS ${user[0].firstName}, you are logged in!!!`}</div> : <div>LOG IN FAILED</div>;
+  const { state } = props.history.location;
+  let user;
+  state && (user = state.user);
+
+  return state && user.length ? (
+    <div>{`GREETINGS ${user[0].firstName}, you are logged in!!!`}</div>
+  ) : (
+    <div>Can't access this page if you're not logged in</div>
+  );
 };
 
 // proptypes to do typechecking
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { loginUser } from '../redux/reducers/user';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
