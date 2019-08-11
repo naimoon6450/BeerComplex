be62864e5da9a24b280b6a9fbc869fb3dc699a20
Dylan Levene <levene.dylan@gmail.com>
2019-08-10 23:19:33 -0400

    one more thing

--- src/redux/reducers/product.js
+++ src/redux/reducers/product.js
@@ -51,7 +51,7 @@ export const fetchProduct = productId => dispatch => {
   dispatch(fetchingProductData());
   axios
     .get(`/api/products/${productId}`)
-    .then((({data}) => dispatch(getProduct(data))))
+    .then(({data}) => dispatch(getProduct(data)))
     .catch(error => console.error(error));
 };
 
